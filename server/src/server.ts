4ad65725b338b40632cbf0dd610661968cdc4acf
Nicolas Alejandro Vaquero <nvaquero@est.frba.utn.edu.ar>
2019-09-15 23:38:51 -0300

    Adds HTTPS and Dev certificate

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
-// Load environment variables from .env file
-import dotenv from "dotenv";
-dotenv.config();
-
-import { createServer } from "http";
+import { resolve } from "path";
+import { readFileSync } from "fs";
+import { createServer } from "https";
 import { logger } from "./helpers/logger";
 import app from "./serve";
 
-// ToDo: Add HTTPS with certificates
-const server = createServer({}, app);
+const server = createServer({
+	key: readFileSync(resolve("./certs/key.pem")),
+	cert: readFileSync(resolve("./certs/cert.pem")),
+}, app);
 
 server.listen(process.env.PORT, () => {
 	logger.debug(`App is running at http://localhost:${process.env.PORT} in ${process.env.NODE_ENV} mode`);
