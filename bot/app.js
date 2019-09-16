f82a0f18f627f1505cda97318363564085fedf67
Nicolas Alejandro Vaquero <nvaquero@est.frba.utn.edu.ar>
2019-09-15 23:08:23 -0300

    Adds bot

---
+++ bot/app.js
@@ -0,0 +1,30 @@
+const request = require("request");
+const io = require("socket.io-client");
+
+const url = `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_URL}:${process.env.SERVER_PORT}`;
+const socket = io(url);
+socket.on("connect", () => {});
+
+socket.on("chatroom created", (chatroomID) => {
+    const chatroom = io(`${url}/${chatroomID}`);
+    chatroom.emit("bot add", {});
+    chatroom.on("stock", (stock) => {
+        request.get(`https://stooq.com/q/l/?s=${stock}&f=sd2t2ohlcv&h&e=csv`, {}, (error, response) => {
+            const [headersLine, ...dataLines] = response.body.split("\r\n");
+            const properties = headersLine.split(",");
+            const stocks = dataLines.map((aStockString) => {
+                if (aStockString != "") {
+                    const splittedStockString = aStockString.split(",");
+                    return properties.reduce(
+                        (aStock, aProperty, anIndex) => {
+                            aStock[aProperty] = splittedStockString[anIndex];
+                            return aStock;
+                        },
+                        {}
+                    );
+                }
+            }, []);
+            chatroom.emit("message", { text: `${stocks[0].Symbol} quote is ${stocks[0].Close} per share` });
+        });
+    });
+});
